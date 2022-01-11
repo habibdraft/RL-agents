8c9e0ccdae12698b379d2cdedc6e134df3490eb7
Nazia Habib <habib.nazia@gmail.com>
2022-01-11 12:38:52 -0600

    Update agents.py

--- agents.py
+++ agents.py
@@ -1,8 +1,8 @@
 import random
 
 class Agent:
-    def __init__(self, actions, epsilon=0.7, alpha=0.1, gamma=0.1, epsilon_decay=0.9):
-        self.q = {}
+    def __init__(self, states, actions, epsilon=0.7, alpha=0.7, gamma=0.7, epsilon_decay=0.9):
+        self.q = [[0]*actions]*states
         self.actions = [i for i in range(actions)]
         self.epsilon = epsilon
         self.alpha = alpha
@@ -12,12 +12,19 @@ class Agent:
         self.last_action = None
         self.last_reward = None
     
-    def update(self, state, action, reward, new_value):
+    def update2(self, state, action, reward, newValue):
         current_q = self.get_q(state, action)
         if current_q == None:
             self.q[(state, action)] = reward 
         else:
-            self.q[(state, action)] = current_q + self.alpha * (new_value - current_q)
+            self.q[(state, action)] = current_q + self.alpha * (newValue - current_q)
+    
+    def update(self, state, action, reward, new_value):
+        current_q = self.get_q(state, action)
+        if current_q == 0:
+            self.set_q(state, action, reward) 
+        else:
+            self.set_q(state, action, current_q + self.alpha * (new_value - current_q))
             
     def learn(self, state, action, reward, state2, action2):
         pass
@@ -26,22 +33,28 @@ class Agent:
         if random.random() < self.epsilon:
             action = random.choice(self.actions)
         else:
-            q = [self.get_q(state, a) for a in self.actions] 
-            max_q = max(q)
-            count = q.count(max_q)
+            state_q = [self.get_q(state, a) for a in self.actions] 
+            max_q = max(state_q)
+            count = state_q.count(max_q)
             if count > 1:
-                max_actions = [a for a in self.actions if q[a] == max_q] 
+                max_actions = [a for a in self.actions if state_q[a] == max_q] 
                 index = random.choice(max_actions)
             else:
-                index = q.index(max_q)
+                index = state_q.index(max_q)
 
             action = self.actions[index]
             self.epsilon = self.epsilon * self.epsilon_decay
             
         return action
     
-    def get_q(self, state, action):
+    def get_q2(self, state, action):
         return self.q.get((state, action), 0.0)
+    
+    def get_q(self, state, action):
+        return self.q[state][action]
+
+    def set_q(self, state, action, update_value):
+        self.q[state][action] = update_value
 
 class SarsaAgent(Agent):
     
