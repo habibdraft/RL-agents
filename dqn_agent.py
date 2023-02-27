a17637d48f0d3c6d9b6e35a0bdbc6e20511d1e9c
Nazia Habib <habib.nazia@gmail.com>
2023-02-27 11:10:08 -0600

    Update dqn_agent.py

--- dqn_agent.py
+++ dqn_agent.py
@@ -1,1 +1,11 @@
-class Agent
+class Agent:
+  def __init__(self, states, actions, epsilon=0.7, alpha=0.7, gamma=0.7, epsilon_decay=0.9):
+          self.q = [[0]*actions]*states
+          self.actions = [i for i in range(actions)]
+          self.epsilon = epsilon
+          self.alpha = alpha
+          self.gamma = gamma
+          self.epsilon_decay = epsilon_decay
+          self.last_state = None
+          self.last_action = None
+          self.last_reward = None
